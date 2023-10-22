086005132a7287411b9d57fae587b37ea9a88dc6
Alfred <zczhang24@gmail.com>
2023-10-22 22:22:36 +0800

    features 1. Enable using CLAM as a module 2. Add the option multi_pt for pt contour detection.

--- create_patches_fp.py
+++ create_patches_fp.py
@@ -1,7 +1,7 @@
 # internal imports
-from wsi_core.WholeSlideImage import WholeSlideImage
-from wsi_core.wsi_utils import StitchCoords
-from wsi_core.batch_process_utils import initialize_df
+from .wsi_core.WholeSlideImage import WholeSlideImage
+from .wsi_core.wsi_utils import StitchCoords
+from .wsi_core.batch_process_utils import initialize_df
 # other imports
 import os
 import numpy as np
